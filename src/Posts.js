ce852d8ec9a0197cf1d1c6305ab1f4d2147206d1
lupus-yonderboy <lupus_yonderboy@protonmail.com>
2020-01-28 01:52:20 -0500

    fetch posts

--- src/Posts.js
+++ src/Posts.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import { Container } from './Container';
 
-export const Posts = () => {
-  return (
-    <Container>
-      laughing out loud.
-    </Container>
-  );
+const posts = [];
+
+export class Posts extends Component {
+  componentDidMount() {
+    fetch('http://lupus-yonderboy-go-env.wv5mqwfbqj.us-east-1.elasticbeanstalk.com/posts')
+      .then((res) => {
+        return res.json();
+      })
+      .then((json) => {
+        console.log(json);
+      })
+  }
+
+  render() {
+    return (
+      <Container>
+        laughing out loud.
+      </Container>
+    );
+  }
 };
