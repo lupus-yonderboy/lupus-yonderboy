71ab31389a9411a0094db87aab95918f9c4ad023
lupus-yonderboy <lupus_yonderboy@protonmail.com>
2020-02-07 17:11:58 -0500

    Updates

--- precache-manifest.404ce42a5f2e7cdaa28fcdfa607d9990.js
+++ precache-manifest.404ce42a5f2e7cdaa28fcdfa607d9990.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "db5b86054ff4bd4994197f4b18ad72c2",
+    "revision": "fb2168185dc8db849a9411a07fa39787",
     "url": "/lupus-yonderboy/index.html"
   },
   {
-    "revision": "304c5e643a8b5fa7a60c",
+    "revision": "1950d1dabd0f09476fcf",
     "url": "/lupus-yonderboy/static/css/main.68210992.chunk.css"
   },
   {
-    "revision": "27f9a83cd3e3e7f48842",
-    "url": "/lupus-yonderboy/static/js/2.0c026a55.chunk.js"
+    "revision": "faa51cba298cc3404ccc",
+    "url": "/lupus-yonderboy/static/js/2.ce8cc27d.chunk.js"
   },
   {
     "revision": "e928fe768baa9832b5bc57eae021f30c",
-    "url": "/lupus-yonderboy/static/js/2.0c026a55.chunk.js.LICENSE"
+    "url": "/lupus-yonderboy/static/js/2.ce8cc27d.chunk.js.LICENSE"
   },
   {
-    "revision": "304c5e643a8b5fa7a60c",
-    "url": "/lupus-yonderboy/static/js/main.f36fd552.chunk.js"
+    "revision": "1950d1dabd0f09476fcf",
+    "url": "/lupus-yonderboy/static/js/main.a5f24d26.chunk.js"
   },
   {
     "revision": "3a9d2f3ed0929d7a6a19",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/lupus-yonderboy/precache-manifest.17d84a59ce43d890283526ae0136d9f7.js"
+  "/lupus-yonderboy/precache-manifest.404ce42a5f2e7cdaa28fcdfa607d9990.js"
 );
 
 self.addEventListener('message', (event) => {
